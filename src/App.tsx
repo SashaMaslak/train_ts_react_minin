d788c174ca694e2b1ee91031eb7e44155a3b25c6
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-10-26 14:49:23 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react"
+import { useProducts } from "./hooks/products"
 import { Product } from "./components/Product"
+import { Loader } from "./components/Loader"
+import { ErrorMessage } from "./components/ErrorMessage"
+import { Modal } from "./components/Modal"
 
 function App() {
+  const { products, error, loading } = useProducts()
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product />
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map(product => (
+        <Product product={product} key={product.id} />
+      ))}
+      <Modal />
     </div>
   )
 }
---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function CreateProduct() {
+  return <div>CreateProduct</div>
+}
