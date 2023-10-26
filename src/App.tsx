46ade8786894228271209694a91cc5f91e2f6901
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-10-26 17:00:12 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import { useProducts } from "./hooks/products"
 import { Product } from "./components/Product"
 import { Loader } from "./components/Loader"
 import { ErrorMessage } from "./components/ErrorMessage"
 import { Modal } from "./components/Modal"
+import { CreateProduct } from "./components/CreateProduct"
+import { IProduct } from "./models"
 
 function App() {
-  const { products, error, loading } = useProducts()
+  const { products, error, loading, addProduct } = useProducts()
+  const [modal, setModal] = useState(true)
+
+  const createHandler = (product: IProduct) => {
+    setModal(false)
+    addProduct(product)
+  }
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
@@ -14,7 +23,11 @@ function App() {
       {products.map(product => (
         <Product product={product} key={product.id} />
       ))}
-      <Modal />
+      {modal && (
+        <Modal title="Create new product">
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
     </div>
   )
 }
--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,5 +1,66 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
+import { nanoid } from "nanoid"
+import { IProduct } from "../models"
+import { ErrorMessage } from "./ErrorMessage"
 
-export function CreateProduct() {
-  return <div>CreateProduct</div>
+const productData: IProduct = {
+  title: "test product",
+  price: 13.5,
+  description: "lorem ipsum set",
+  image: "https://i.pravatar.cc",
+  category: "electronic",
+  rating: {
+    rate: 20,
+    count: 10,
+  },
+}
+
+interface CreateProductProps {
+  onCreate: (product: IProduct) => void
+}
+
+export function CreateProduct({ onCreate }: CreateProductProps) {
+  const [value, setValue] = useState("")
+  const [error, setError] = useState("")
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setError("")
+
+    if (value.trim().length === 0) {
+      setError("Please enter valid title")
+      return
+    }
+
+    productData.title = value
+    const response = await axios.post<IProduct>(
+      "https://fakestoreapi.com/products",
+      productData
+    )
+
+    onCreate(response.data)
+  }
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        value={value}
+        onChange={event => changeHandler(event)}
+      />
+
+      {error && <ErrorMessage error={error} />}
+
+      <button className="py-2 px-4 border bg-yellow-400 rounded hover:text-white">
+        Create
+      </button>
+    </form>
+  )
 }
