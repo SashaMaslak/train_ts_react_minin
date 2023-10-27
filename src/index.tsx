afe794324537ca1981e00e0f71ebc54c5d69f749
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-10-27 13:49:58 +0300

    6

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client"
 import { ModalState } from "./context/ModalContext"
+import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <ModalState>
-    <App />
-  </ModalState>
+  <BrowserRouter>
+    <ModalState>
+      <App />
+    </ModalState>
+  </BrowserRouter>
 )
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, expedita
+      commodi. Eligendi id saepe, eius tempore corrupti, voluptatem sapiente
+      nemo ullam impedit cumque sunt possimus libero distinctio ipsam eos
+      provident velit molestias aperiam tenetur. Cumque officiis autem ratione
+      odit tenetur veritatis impedit voluptates magni a praesentium. Aspernatur
+      temporibus error nihil.
+    </div>
+  )
+}
